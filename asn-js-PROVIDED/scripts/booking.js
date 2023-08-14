087b87b2d158445b06efb836e9b58a876f9a6ada
JOTSAROOP SINGH <jotsaroop0053@gmail.com>
2023-08-14 15:47:04 -0600

    intial commit

---
+++ asn-js-PROVIDED/scripts/booking.js
@@ -0,0 +1,49 @@
+/********* create variables *********/
+const cost_per_day_full = 35;
+const cost_per_day_half = 20;
+let numberOfDaysSelected = 0;
+let dailyRate = cost_per_day_full;
+const day_selector_items = document.querySelectorAll('.day-selector li');
+const full_button = document.getElementById('full');
+const half_button = document.getElementById('half');
+const calculatedCostSpan = document.getElementById('calculated-cost');
+const clearbutton = document.getElementById('clear-button');
+/********* calculate *********/
+document.addEventListener("DOMContentLoaded", function() {
+    function updateCost() {
+        calculatedCostSpan.textContent = dailyRate * numberOfDaysSelected;
+    }
+    /********* colour change days of week *********/
+    day_selector_items.forEach(item => {
+        item.addEventListener('click', function() {
+            if (!item.classList.contains('clicked')) {
+                numberOfDaysSelected++;
+                item.classList.add('clicked');
+                updateCost();
+            }
+        });
+    });
+    /********* clear days *********/
+    clearbutton.addEventListener('click', function() {
+        day_selector_items.forEach(item => {
+            item.classList.remove('clicked');
+        });
+        full_button.classList.remove('clicked');
+        half_button.classList.remove('clicked');
+        numberOfDaysSelected = 0; 
+        updateCost();
+    });
+    /********* change rate *********/
+    half_button.addEventListener('click', function() {
+        dailyRate = cost_per_day_half;
+        half_button.classList.add('clicked');
+        full_button.classList.remove('clicked');
+        updateCost();
+    });
+    full_button.addEventListener('click', function() {
+        dailyRate = cost_per_day_full;
+        full_button.classList.add('clicked');
+        half_button.classList.remove('clicked');
+        updateCost();
+    });
+});
---
+++ asn-js-PROVIDED/scripts/contact.js
@@ -0,0 +1,14 @@
+// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+
+// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+
+var SUBMITBUTTON = document.getElementById("submit-button");
+var contact = document.getElementById("contact-page");
+function thanks() {
+    contact.innerHTML = '';
+    const output = document.createElement("p");
+    output.innerHTML = "Thank you for your message!";
+    output.style.fontSize = '24px';
+    contact.append(output);
+}
+SUBMITBUTTON.addEventListener("click", thankyou);
